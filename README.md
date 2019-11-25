61bbd534f2927f5b8847a6649e4ffd4b9cddda3a
BrunoMine <borgesdossantosbruno@gmail.com>
2019-11-25 14:17:21 -0300

    Add sounds and eat mode

--- README.md
+++ README.md
@@ -17,6 +17,18 @@ See LICENSE.txt for more info about LGPL 3.0 license
 ### Authors of the source code
 Originally by BrunoMine, Bruno Borges <borgesdossantosbruno@gmail.com> (LGPL 3.0)
 
+gibarroule CC0 (http://freesound.org/people/gibarroule)
+	cow_eat_grass.ogg
+	cow_eat_grass.2.ogg
+	cow_eat_grass.3.ogg
+
+josephsardin CC0 (http://freesound.org/people/josephsardin)
+	cow_idle.ogg
+	cow_idle.2.ogg
+	cow_idle.3.ogg
+	cow_idle.4.ogg
+	cow_idle.5.ogg
+
 ### Authors of media (textures, models and sonds)
 All that are not described here are authored by 
 BrunoMine, Bruno Borges <borgesdossantosbruno@gmail.com> (CC BY-SA 3.0)
--- cow.lua
+++ cow.lua
@@ -44,13 +44,26 @@ creatures.register_mob("cow:cow", {
 		walk = {
 			chance = 0.14, 
 			duration = 20, 
-			moving_speed = 1.3,
+			moving_speed = 0.8,
 			search_radius = 5
 		},
 		walk_around = {
 			chance = 0.2, 
 			duration = 20, 
 			moving_speed = 0.7
+		},
+		eat = {	chance = 1,
+			duration = 4,
+			eat_time = 1.2,
+			sound = "cow_eat_grass",
+			nodes = {
+				["default:grass_1"] = {remove=true}, 
+				["default:grass_2"] = {remove=true}, 
+				["default:grass_3"] = {remove=true}, 
+				["default:grass_4"] = {remove=true}, 
+				["default:grass_5"] = {remove=true}, 
+				["default:dirt_with_grass"] = {replace="default:dirt"}, 
+			}
 		},
 	},
 	
@@ -63,11 +76,11 @@ creatures.register_mob("cow:cow", {
 		scale = {x = 3.5, y = 3.5},
 		vision_height = 0.9,
 		animations = {
-			idle = {start = 1, stop = 60, speed = 15},
-			walk = {start = 81, stop = 101, speed = 18},
-			eat = {start = 107, stop = 170, speed = 12, loop = false},
-			follow = {start = 81, stop = 101, speed = 15},
-			death = {start = 171, stop = 191, speed = 32, loop = false, duration = 2.52},
+			idle = {start = 1, stop = 30, speed = 18},
+			walk = {start = 31, stop = 60, speed = 20},
+			run = {start = 91, stop = 120, speed = 20},
+			eat = {start = 61, stop = 90, speed = 12, loop = false},
+			death = {start = 121, stop = 135, speed = 15, loop = false, duration = 2.52},
 		},
 	},
 
@@ -76,7 +89,7 @@ creatures.register_mob("cow:cow", {
 		on_death = {name = "sheep", gain = 1.0, distance = 10},
 		swim = {name = "creatures_splash", gain = 1.0, distance = 10,},
 		random = {
-			idle = {name = "sheep", gain = 0.6, distance = 10, time_min = 23},
+			idle = {name = "cow_idle", gain = 0.6, distance = 10, time_min = 23},
 		},
 	},
 	
