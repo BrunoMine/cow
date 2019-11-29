fe8768e79eef0563d5a4fc69c0f28d1546a826bb
BrunoMine <borgesdossantosbruno@gmail.com>
2019-11-29 13:12:32 -0300

    Remove spawn node from creative inventory

--- cow.lua
+++ cow.lua
@@ -11,7 +11,7 @@
 minetest.register_node("cow:dirt_spawn_env", {
 	description = "Dirt",
 	tiles = {"default_dirt.png"},
-	groups = {crumbly = 3, soil = 1},
+	groups = {crumbly = 3, soil = 1, not_in_creative_inventory = 1},
 	sounds = default.node_sound_dirt_defaults(),
 	drop = "default:dirt",
 })
